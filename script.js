00a50931074528b1726a5f1e4630d174f2971e53
Jacob <Jakelausier7878@gmail.com>
2023-06-29 17:09:20 -0400

    fixed math floor and pushed result into a new array

--- script.js
+++ script.js
@@ -14,9 +14,9 @@
 
 //////////////////////////////////////////////////
 
-// Number array lower
+//  lower case array
 const lowLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-//Number array Upper
+//uppere case array
 const tallLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 //Numeric array
 const numbers = [1,2,3,4,5,6,7,8,9,0]
@@ -33,17 +33,17 @@ var confirmspecial;
 // if (passOBj.count >=8 || passObj.count <= 128) {
 //   alert("Would you like Lower case Letters in your password?")
 let passOBj = [""]
-let password = [""]
+let password = []
 
 //function generate password
 function generatePassword (){
    //hit btn prompt how many chars
-const passOBj =prompt("How many charecters long would you like your password?");
+let passOBj =parseInt (prompt("How many charecters long would you like your password?"));
 console.log(passOBj);
 console.log(typeof passOBj);
 
 // gather input value
- if  (parseInt(passOBj) < 8 || parseInt(passOBj) > 128){
+ if  ((passOBj) < 8 || (passOBj) > 128){
     alert ("That number is not between 8 and 128. Please enter a correct number")
 } else {
   // All the confirms
@@ -53,8 +53,43 @@ console.log(typeof passOBj);
    confirmtallletter = confirm("Would you like Upper case letters in your password?")
    confirmnumbers = confirm("Would you like Numbers in your password?")
    confirmspecial = confirm("Would you like Special charecters in your password?")
+
+   
+
+if(confirmlowLetter){
+  password = password.concat(lowLetter)
 }
+// do the same check for every confirm
+if(confirmtallletter){
+  password = password.concat(tallLetter)
+  
 }
+
+if(confirmnumbers){
+  password = password.concat(numbers)
+}
+if(confirmspecial){
+  password = password.concat(special)
+  console.log(password);
+} 
+// test to see if we get concat array- note we may need to make a final array to concat too
+let newPassword = []
+// once we have a final array we can loop over ist however many times the user chose and get a random value out with math.random
+for (var index = 0; index < passOBj; index++) {
+  newPassword = newPassword.concat(password[Math.floor(Math.random() * password.length)]);
+console.log(newPassword);
+}
+
+return newPassword;
+
+}
+  
+}
+
+// look at the join method 
+
+// finally return the joined string ass the password
+
 
 
  
